3be3373163b5656ba5a03402a294412e15fee2a7
Stahann <stahankhmil@gmail.com>
2024-01-04 16:12:23 +0200

    updated new key for API

--- src/services/cryptoNewsApi.ts
+++ src/services/cryptoNewsApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoNewsHeaders = {
-    'X-RapidAPI-Key': '0cf026dcbemsh6c3e338c44f81d8p1b404djsn3c6ed56cfa28',
+    'X-RapidAPI-Key': 'bd0407e96amsh89611f3835585e7p190611jsn1578948ca2d2',
     'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com',
 }
 
@@ -19,9 +19,9 @@ export const cryptoNewsApi = createApi({
     endpoints: (builder) => ({
         getCryptoNews: builder.query<
             { data: any[] },
-            { limit: number | undefined }
+            { limit: number | undefined; url: string }
         >({
-            query: (args) => createRequest('/coindesk', args),
+            query: (args) => createRequest(args.url, args),
             transformResponse: (rawResult: { data: any[] }, meta) => {
                 const url = meta?.request.url || ''
 
